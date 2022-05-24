91c12e60ae09d9c43eade736d25b99ae5981134f
kdabros <jakub.b.dabros@gmail.com>
2022-05-24 17:23:15 +0200

    Further testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
-int main()
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
 {
-  std::cout << "cmake_test_action hello world!" << std::endl;
-  return 0;
+    QGuiApplication app(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "Learning_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            app.installTranslator(&translator);
+            break;
+        }
+    }
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/Learning/main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
 }
